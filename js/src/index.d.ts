8bfe221027df3d07c1d23ed88f1056c69074a769
Maxim Andreev <andreevmaxim@gmail.com>
2023-12-13 21:25:03 +0300

    js: fix d.ts typescript

--- js/src/index.d.ts
+++ js/src/index.d.ts
@@ -1,4 +1,2 @@
-export function functionSelectors(
-  code_hex_string: string,
-  gas_limit: number = 1e6,
-)
+export declare function functionSelectors(code_hex_string: string, gas_limit?: number): string[];
+export declare function functionArguments(code_hex_string: string, selector_hex_string: string, gas_limit?: number): string;
