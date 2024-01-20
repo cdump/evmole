95fbf58c327b5a2cf6a1dc4c48cd12b9b21a5cda
Maxim Andreev <andreevmaxim@gmail.com>
2024-01-20 18:32:04 +0300

    python: fix test after Element migration

--- tests/memory_test.py
+++ tests/memory_test.py
@@ -1,34 +1,36 @@
+from evmole.evm.element import Element
 from evmole.evm.memory import Memory
+
 
 def test_memory():
     m = Memory()
 
-    m.store(0, b'\xaa')
-    assert m.load(0) == (b'\xaa' + b'\x00'*31, {b'\xaa'})
+    m.store(0, Element(b'\xaa', 'l1'))
+    assert m.load(0) == (b'\xaa' + b'\x00'*31, {'l1'})
     assert m.load(1) == (b'\x00'*32, set())
 
-    m.store(8, b'\xbb')
-    assert m.load(0) == (b'\xaa' + b'\x00'*7 + b'\xbb' + b'\x00'*23, {b'\xaa', b'\xbb'})
-    assert m.load(2) == (b'\x00'*6 + b'\xbb' + b'\x00'*25, {b'\xbb'})
-    assert m.load(8) == (b'\xbb' + b'\x00'*31, {b'\xbb'})
+    m.store(8, Element(b'\xbb', 'l2'))
+    assert m.load(0) == (b'\xaa' + b'\x00'*7 + b'\xbb' + b'\x00'*23, {'l1', 'l2'})
+    assert m.load(2) == (b'\x00'*6 + b'\xbb' + b'\x00'*25, {'l2'})
+    assert m.load(8) == (b'\xbb' + b'\x00'*31, {'l2'})
     assert m.load(9) == (b'\x00'*32, set())
 
-    m.store(0, b'\xcc')
-    assert m.load(0) == (b'\xcc' + b'\x00'*7 + b'\xbb' + b'\x00'*23, {b'\xcc', b'\xbb'})
+    m.store(0, Element(b'\xcc')) # without label
+    assert m.load(0) == (b'\xcc' + b'\x00'*7 + b'\xbb' + b'\x00'*23, {'l2'})
 
 
     m = Memory()
-    m.store(4, b'\xaa\xbb')
-    m.store(3, b'\xcc')
-    assert m.load(0) == (b'\x00' * 3 + b'\xcc\xaa\xbb' + b'\x00'*26, {b'\xaa\xbb', b'\xcc'})
+    m.store(4, Element(b'\xaa\xbb', 'l1'))
+    m.store(3, Element(b'\xcc', 'l2'))
+    assert m.load(0) == (b'\x00' * 3 + b'\xcc\xaa\xbb' + b'\x00'*26, {'l1', 'l2'})
 
-    m.store(3, b'\xdd\xee')
-    assert m.load(0) == (b'\x00' * 3 + b'\xdd\xee\xbb' + b'\x00'*26, {b'\xaa\xbb', b'\xdd\xee'})
+    m.store(3, Element(b'\xdd\xee', 'l3'))
+    assert m.load(0) == (b'\x00' * 3 + b'\xdd\xee\xbb' + b'\x00'*26, {'l1', 'l3'})
 
 
     m = Memory()
-    m.store(3, b'\xaa\xbb\xcc')
-    m.store(4, b'\xdd')
-    assert m.load(0) == (b'\x00' * 3 + b'\xaa\xdd\xcc' + b'\x00'*26, {b'\xaa\xbb\xcc', b'\xdd'})
-    m.store(2, b'\xee\xff')
-    assert m.load(0) == (b'\x00' * 2 + b'\xee\xff\xdd\xcc' + b'\x00'*26, {b'\xaa\xbb\xcc', b'\xdd', b'\xee\xff'})
+    m.store(3, Element(b'\xaa\xbb\xcc', 'l1'))
+    m.store(4, Element(b'\xdd', 'l2'))
+    assert m.load(0) == (b'\x00' * 3 + b'\xaa\xdd\xcc' + b'\x00'*26, {'l1', 'l2'})
+    m.store(2, Element(b'\xee\xff', 'l3'))
+    assert m.load(0) == (b'\x00' * 2 + b'\xee\xff\xdd\xcc' + b'\x00'*26, {'l1', 'l2', 'l3'})
