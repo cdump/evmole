0fe987062456a3a9eda3d089ce7da446d7812b41
Maxim Andreev <andreevmaxim@gmail.com>
2024-08-30 11:39:31 +0000

    update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,51 +1,20 @@
 #!/bin/sh
 
+MODE=$1
 DS=$2
 
 BDIR=`pwd`/benchmark
 
-# MODE=selectors
-MODE=arguments
+ln -s `pwd` ${BDIR}/providers/evmole-rs/rust 2>/dev/null || true
 
-case $1 in
-    js)
-        echo "TODO" && exit 1
-        node ${BDIR}/providers/evmole-js/main.mjs \
-            ${MODE} \
-            ${BDIR}/datasets/${2} \
-            out.json \
-            ${BDIR}/results/etherscan.selectors_${2}.json \
-            --filter-filename ${3} \
-            --filter-selector ${4}
-    ;;
+cargo run \
+    --manifest-path benchmark/providers/evmole-rs/Cargo.toml \
+    --features "evmole/trace" \
+    ${MODE} \
+    ${BDIR}/datasets/${2} \
+    out.json \
+    ${BDIR}/results/etherscan.selectors_${2}.json \
+    --filter-filename ${3} \
+    --filter-selector ${4}
 
-    rs)
-        ln -s `pwd` ${BDIR}/providers/evmole-rs 2>/dev/null || true
-        cargo run \
-            --manifest-path benchmark/providers/evmole-rs/Cargo.toml \
-            --features "evmole/trace" \
-            ${MODE} \
-            ${BDIR}/datasets/${2} \
-            out.json \
-            ${BDIR}/results/etherscan.selectors_${2}.json \
-            --filter-filename ${3} \
-            --filter-selector ${4}
-    ;;
-
-    py)
-        echo "TODO" && exit 1
-        PYTHONPATH=`pwd` \
-            python3.12 \
-            ${BDIR}/providers/evmole-py/main.py \
-            ${MODE} \
-            ${BDIR}/datasets/${2} \
-            out.json \
-            ${BDIR}/results/etherscan.selectors_${2}.json \
-            --filter-filename ${3} \
-            --filter-selector ${4}
-    ;;
-
-    *)
-        echo 'unknown "$1"'
-        exit 1;
-esac
+rm -rf ${BDIR}/providers/evmole-rs/rust
