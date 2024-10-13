4fbde03149479305319baf3e533a4fed68504682
Maxim Andreev <andreevmaxim@gmail.com>
2024-10-13 10:16:35 +0300

    js examples: add vite dev build bug workaround

--- javascript/examples/vite/vite.config.js
+++ javascript/examples/vite/vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     target: 'esnext',
-  }
+  },
+  optimizeDeps: { // https://github.com/vitejs/vite/issues/13756
+    exclude: ['evmole'],
+  },
 })
