6d4548a7682517b054b9f9cd67840df225765904
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2025-05-19 19:25:43 +0200

    chore(deps): bump alloy-primitives to 1.0, use alloy_primitives::map (#25)

--- src/collections.rs
+++ src/collections.rs
@@ -1,24 +1,5 @@
-use ahash::RandomState;
+type HashBuilder = alloy_primitives::map::foldhash::fast::FixedState;
 
-#[derive(Clone)]
-pub(crate) struct MyHashBuilder {
-    state: RandomState,
-}
-
-impl Default for MyHashBuilder {
-    fn default() -> Self {
-        Self {
-            state: RandomState::with_seed(42),
-        }
-    }
-}
-impl std::hash::BuildHasher for MyHashBuilder {
-    type Hasher = <RandomState as std::hash::BuildHasher>::Hasher;
-    fn build_hasher(&self) -> Self::Hasher {
-        self.state.build_hasher()
-    }
-}
-
-pub(crate) type HashMap<K, V> = std::collections::HashMap<K, V, MyHashBuilder>;
-pub(crate) type HashSet<K> = std::collections::HashSet<K, MyHashBuilder>;
-pub(crate) type IndexMap<K, V> = indexmap::IndexMap<K, V, MyHashBuilder>;
+pub(crate) type HashMap<K, V> = std::collections::HashMap<K, V, HashBuilder>;
+pub(crate) type HashSet<K> = std::collections::HashSet<K, HashBuilder>;
+pub(crate) type IndexMap<K, V> = indexmap::IndexMap<K, V, HashBuilder>;
